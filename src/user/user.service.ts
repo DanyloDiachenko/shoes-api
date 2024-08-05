ff21f4358407b78c9bc7aa81cae322768a88396a
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-08-06 00:00:41 +0300

    user service & argon2

---
+++ src/user/user.service.ts
@@ -0,0 +1,40 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { UserEntity } from "./entites/user.entity";
+import { JwtService } from "@nestjs/jwt";
+import { CreateUserDto } from "./dto/create-user.dto";
+import * as argon2 from "argon2";
+
+@Injectable()
+export class UserService {
+    constructor(
+        private readonly userRepository: Repository<UserEntity>,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    async create(createUserDto: CreateUserDto) {
+        const existingUser = await this.userRepository.findOne({
+            where: {
+                email: createUserDto.email,
+            },
+        });
+
+        if (existingUser) {
+            throw new BadRequestException("This email already exist");
+        }
+
+        const createdUser = await this.userRepository.save({
+            email: createUserDto.email,
+            passwordHash: await argon2.hash(createUserDto.password),
+        });
+
+        const token = this.jwtService.sign({
+            email: createUserDto.email,
+        });
+
+        return {
+            createdUser,
+            token,
+        };
+    }
+}
