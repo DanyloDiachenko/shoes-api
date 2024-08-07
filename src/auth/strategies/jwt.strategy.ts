2ef603006d68763e91a91e7333a9f4617aee79fb
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-08-08 00:13:04 +0300

    fix

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { UserEntity } from "src/users/entites/user.entity";
+import { IUser } from "types/user.interface";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         });
     }
 
-    async validate(user: UserEntity) {
+    async validate(user: IUser) {
         return {
             id: user.id,
             email: user.email,
--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,14 +1,12 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-jwt";
+import { Strategy } from "passport-local";
 import { AuthService } from "../auth.service";
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-    constructor(private readonly authService: AuthService) {
-        super({
-            usernameField: "email",
-        });
+    constructor(private authService: AuthService) {
+        super({ usernameField: "email" });
     }
 
     async validate(email: string, password: string) {
